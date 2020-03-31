125d41d0a3353a9bb679d80ebc8f05f5fa28322e
TerraWest64 <terrawestlp@gmail.com>
2020-03-31 14:05:23 +0200

    Added CsvReader class with the readFile function

---
+++ src/main/java/de/TerraWest/timetableapp/CsvReader.java
@@ -0,0 +1,37 @@
+package de.TerraWest.timetableapp;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class CsvReader {
+    public void readFile() {
+        System.out.println("done");
+
+        String csvFile = "C:\\Users\\Christian\\Documents\\JavaProjects\\Timetable-App\\src\\main\\resources\\Stundenplan 19-20.csv";
+        String line = "";
+        String csvSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as seperator
+                String[] row = line.split(csvSplitBy);
+                //String timetable = String.format("");
+                System.out.println(
+                         row [0] + " | "
+                        +row [1] + " | "
+                        +row [2] + " | "
+                        +row [3] + " | "
+                        +row [4] + " | "
+                        //+row [5] + " "
+                        //+row [6] + " "
+                );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/de/TerraWest/timetableapp/Main.java
+++ src/main/java/de/TerraWest/timetableapp/Main.java
@@ -3,6 +3,7 @@ package de.TerraWest.timetableapp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int i = 1;
+        var CsvReader = new CsvReader();
+        CsvReader.readFile();
     }
 }
