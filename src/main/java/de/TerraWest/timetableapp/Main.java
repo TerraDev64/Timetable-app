f66fed495fcde27d15c65431cb080af7f9518b96
TerraWest64 <terrawestlp@gmail.com>
2020-04-07 21:27:54 +0200

    removed spaces from Stundenplan; Added message for IOException; chang

--- src/main/java/de/TerraWest/timetableapp/Main.java
+++ src/main/java/de/TerraWest/timetableapp/Main.java
@@ -2,7 +2,6 @@ package de.TerraWest.timetableapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         var CsvReader = new CsvReader();
         CsvReader.readFile();
     }
