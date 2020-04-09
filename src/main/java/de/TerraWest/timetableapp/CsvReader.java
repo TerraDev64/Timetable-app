bfc934d8409658f7c4cf3c793bab83be2d4bd2ba
TerraWest64 <terrawestlp@gmail.com>
2020-04-09 17:26:29 +0200

    added parameters in showFile() and readFile()

--- src/main/java/de/TerraWest/timetableapp/CsvReader.java
+++ src/main/java/de/TerraWest/timetableapp/CsvReader.java
@@ -3,15 +3,23 @@ package de.TerraWest.timetableapp;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.nio.Buffer;
 
 public class CsvReader {
-    public void readFile() {
 
-        final String csvFile = "";
+    public void readFile(String csvFile) {
         final String csvSplitBy = ",";
 
+        try (var br = new BufferedReader(new FileReader(csvFile))) {
+            String line = "";
 
+
+        } catch (IOException eIO) {
+            System.out.println("File not Found " + eIO);
+        }
+    }
+    //Parameters for using variables from other functions
+    public void showFile(String csvFile, String csvSplitBy) {
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             String line = "";
@@ -22,12 +30,12 @@ public class CsvReader {
                 String[] row = line.split(csvSplitBy);
                 //String timetable = String.format("");
                 System.out.println(
-                         row [0]
-                        +row [1]
-                        +row [2]
-                        +row [3]
-                        +row [4]
-                        +row [5]
+                                 row [0]
+                                +row [1]
+                                +row [2]
+                                +row [3]
+                                +row [4]
+                                +row [5]
 
                 );
             }
--- src/main/java/de/TerraWest/timetableapp/Main.java
+++ src/main/java/de/TerraWest/timetableapp/Main.java
@@ -1,8 +1,12 @@
 package de.TerraWest.timetableapp;
 
+
 public class Main {
     public static void main(String[] args) {
         var CsvReader = new CsvReader();
-        CsvReader.readFile();
+        //var showFile = new show
+        System.out.println(System.getProperty("file"));
+        CsvReader.readFile(System.getProperty("file"));
+
     }
 }
