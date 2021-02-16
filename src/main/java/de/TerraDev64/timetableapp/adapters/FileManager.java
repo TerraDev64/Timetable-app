195ecccfe376ea3e3eaa10d8c027037aa7200739
TerraDev64 <TerraDev64@gmail.com>
2021-02-16 14:57:41 +0100

    added FileManager WIP

---
+++ src/main/java/de/TerraDev64/timetableapp/adapters/FileManager.java
@@ -0,0 +1,24 @@
+package de.TerraDev64.timetableapp.adapters;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileManager {
+    public static String readFile(String csvFile) throws IOException {
+        var file = new File(csvFile);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        var fileContent = new StringBuilder();
+        var br = new BufferedReader(new FileReader(file));
+        var line = "";
+        while ((line = br.readLine()) != null) {
+            fileContent.append(line).append('\n');
+        }
+        return fileContent.toString();
+    }
+}
