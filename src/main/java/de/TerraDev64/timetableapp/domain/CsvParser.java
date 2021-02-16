02e28c9ea58afda77903bcd0296db33714f2493b
TerraDev64 <TerraDev64@gmail.com>
2021-02-16 22:39:44 +0100

    added CsvParser code

---
+++ src/main/java/de/TerraDev64/timetableapp/domain/CsvParser.java
@@ -0,0 +1,12 @@
+package de.TerraDev64.timetableapp.domain;
+
+import java.util.List;
+
+public class CsvParser {
+	private static final String SEPERATOR = ",";
+	private static final String LINE_SEPERATOR = "\n";
+
+	public List parseString(String string) {
+		return null;
+	}
+}
