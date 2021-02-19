3e476ae07bc6012397dc27ceba66eeec4bc7b2ea
TerraDev64 <TerraDev64@gmail.com>
2021-02-20 00:39:36 +0100

    Added stundenplan class

---
+++ src/main/java/de/TerraDev64/timetableapp/adapters/Stundenplan.java
@@ -0,0 +1,30 @@
+package de.TerraDev64.timetableapp.adapters;
+
+import de.TerraDev64.timetableapp.domain.StundenplanRepository;
+
+public class Stundenplan implements StundenplanRepository {
+	@Override
+	public void addNewHour() {
+
+	}
+
+	@Override
+	public void deleteHour() {
+
+	}
+
+	@Override
+	public void deleteStundenplan() {
+
+	}
+
+	@Override
+	public void getData() {
+
+	}
+
+	@Override
+	public String[] getStundenplan() {
+		return new String[0];
+	}
+}
